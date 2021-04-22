a07adc5cbd083861f5f2da1e2a183aadb5b3e47c
PrikrylovaJ <jprikrylova@email.cz>
2021-04-22 15:01:17 +0200

    Výběr symbolů pomocí background-image

--- hra.js
+++ hra.js
@@ -5,22 +5,22 @@ let btnElements = document.querySelectorAll('.btn');
 const iconElm = document.querySelector('.icon-left');
 
 const hra = (event) => {
-  if (event.target.innerHTML !== `<img src="images/circle.svg">` && event.target.innerHTML !== `<img src="images/cross.svg">`) {
+  // if (event.target.innerHTML !== `<img src="images/circle.svg">` && event.target.innerHTML !== `<img src="images/cross.svg">`) {
     if (kdoJeNaTahu === 'circle') {
     event.target.classList.add('board__field--circle');
-    event.target.innerHTML = `<img src="images/circle.svg">`;
+    // event.target.innerHTML = `<img src="images/circle.svg">`;
     kdoJeNaTahu = 'cross';
     iconElm.src = 'images/cross.svg';
     } else if (kdoJeNaTahu === 'cross') {
     event.target.classList.add('board__field--cross');
-    event.target.innerHTML = `<img src="images/cross.svg">`;
+    // event.target.innerHTML = `<img src="images/cross.svg">`;
     kdoJeNaTahu = 'circle';
     iconElm.src = 'images/circle.svg';
     } else {
     event.target.style.display ='none';
   } 
 }
-}
+// }
 
 for (let i = 0; i < btnElements.length; i++) {
   const btnElm = btnElements[i];
