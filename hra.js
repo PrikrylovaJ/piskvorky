25884e58cc17a2ed3fda49adf089def8dbdaffa0
PrikrylovaJ <jprikrylova@email.cz>
2021-04-22 15:30:16 +0200

    Animace, button disabled

--- hra.js
+++ hra.js
@@ -5,22 +5,20 @@ let btnElements = document.querySelectorAll('.btn');
 const iconElm = document.querySelector('.icon-left');
 
 const hra = (event) => {
-  // if (event.target.innerHTML !== `<img src="images/circle.svg">` && event.target.innerHTML !== `<img src="images/cross.svg">`) {
     if (kdoJeNaTahu === 'circle') {
     event.target.classList.add('board__field--circle');
-    // event.target.innerHTML = `<img src="images/circle.svg">`;
+    event.target.disabled = true;
     kdoJeNaTahu = 'cross';
     iconElm.src = 'images/cross.svg';
     } else if (kdoJeNaTahu === 'cross') {
     event.target.classList.add('board__field--cross');
-    // event.target.innerHTML = `<img src="images/cross.svg">`;
+    event.target.disabled = true;
     kdoJeNaTahu = 'circle';
     iconElm.src = 'images/circle.svg';
     } else {
     event.target.style.display ='none';
   } 
 }
-// }
 
 for (let i = 0; i < btnElements.length; i++) {
   const btnElm = btnElements[i];
