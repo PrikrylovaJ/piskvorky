05b187c39b83a4848c269ba910385f0cf243e1f7
PrikrylovaJ <jprikrylova@email.cz>
2021-05-06 14:09:33 +0200

    Nastavení výherního tahu

--- hra.js
+++ hra.js
@@ -18,9 +18,103 @@ const hra = (event) => {
     } else {
     event.target.style.display ='none';
   } 
+
+  const win = isWinningMove(event.target);
+  if (win === true) {
+    if (kdoJeNaTahu === 'cross') {
+      alert('Hráč kolečko vyhrál');
+    } else {
+      alert(`Hráč křížek výhral`);
+    }
+  }
+
+  // console.log(getSymbol(btnElements[1]));
+  // console.log(getPosition(btnElements[2]));
 }
 
 for (let i = 0; i < btnElements.length; i++) {
-  const btnElm = btnElements[i];
-  btnElm.addEventListener('click', hra);
+  // const btnElm = btnElements[i];
+  btnElements[i].addEventListener('click', hra);
+}
+
+const getSymbol = (field) => {
+	
+	if (field.classList.contains('board__field--cross')) {
+		return 'cross'
+	} else if (field.classList.contains('board__field--circle')) {
+		return 'circle'
+	}
+}
+
+const boardSize = 10 
+
+const getField = (row, column) => {
+  return btnElements[row * boardSize + column];
+}
+
+console.log(getField(0,1));
+
+const getPosition = (field) => {
+	let fieldIndex = 0
+	while (fieldIndex < btnElements.length && field !== btnElements[fieldIndex]) {
+		fieldIndex++
+	}
+
+	return {
+		row: Math.floor(fieldIndex / boardSize),
+		column: fieldIndex % boardSize,
+	}
+}
+
+
+const symbolsToWin = 5
+const isWinningMove = (field) => {
+	const origin = getPosition(field)
+	const symbol = getSymbol(field)
+
+	let i
+
+	let inRow = 1 
+	i = origin.column 
+	while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+		inRow++
+		i--
+	}
+
+	i = origin.column
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(origin.row, i + 1))
+	) {
+		inRow++
+		i++
+	}
+
+	if (inRow >= symbolsToWin) {
+		return true
+	}
+
+	let inColumn = 1
+
+	i = origin.row
+	while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+		inColumn++
+		i--
+	}
+
+	
+	i = origin.row
+	while (
+		i < boardSize - 1 &&
+		symbol === getSymbol(getField(i + 1, origin.column))
+	) {
+		inColumn++
+		i++
+	}
+
+	if (inColumn >= symbolsToWin) {
+		return true
+	}
+
+	return false
 }
